a60989006fbaaf9ecbba872815bfe908762c8a4c
abel tom augustin <155573805+ataugust@users.noreply.github.com>
2025-02-18 20:00:52 +0000

    Florida Hoste l alignment koode onn sheriyakkanam

---
+++ annex.js
@@ -0,0 +1,48 @@
+let aboutTab = document.getElementById("aboutTab");
+let aboutButton = document.getElementById("aboutButton");
+
+let timeToVisitTab = document.getElementById("timeToVisitTab");
+let timeToVisitButton = document.getElementById("timeToVisitButton");
+
+let attractionsTab = document.getElementById("attractionsTab");
+let attractionsButton = document.getElementById("attractionsButton");
+
+timeToVisitTab.classList.add("d-none");
+attractionsTab.classList.add("d-none");
+
+function about() {
+    timeToVisitTab.classList.add("d-none");
+    attractionsTab.classList.add("d-none");
+    aboutTab.classList.remove("d-none");
+    aboutButton.classList.add("selected-button");
+    timeToVisitButton.classList.remove("selected-button");
+    attractionsButton.classList.remove("selected-button");
+    console.log("about is working");
+
+}
+
+function timeToVisit() {
+    timeToVisitTab.classList.remove("d-none");
+    aboutTab.classList.add("d-none");
+    attractionsTab.classList.add("d-none");
+
+    aboutButton.classList.add("button");
+    aboutButton.classList.remove("selected-button");
+    timeToVisitButton.classList.add("selected-button");
+    attractionsButton.classList.add("button");
+    attractionsButton.classList.remove("selected-button");
+}
+
+function attractions() {
+    attractionsTab.classList.remove("d-none");
+    aboutTab.classList.add("d-none");
+    timeToVisitTab.classList.add("d-none");
+
+    aboutButton.classList.add("button");
+    aboutButton.classList.remove("selected-button");
+    timeToVisitButton.classList.add("button");
+    timeToVisitButton.classList.remove("selected-button");
+    attractionsButton.classList.add("selected-button");
+
+
+}
